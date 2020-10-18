1eb4510203f168131432a3f7a1e5be8db17d8ae7
ninzzya <s.a.azarov@gmail.com>
2020-10-18 17:05:47 +0300

    Install and config: babel, webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundel.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    port: 1313,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
