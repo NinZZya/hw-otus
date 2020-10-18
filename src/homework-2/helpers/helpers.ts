c46511506736323dc5444c0d20499fce0e15ae06
ninzzya <s.a.azarov@gmail.com>
2020-10-18 20:23:21 +0300

    Commit isNumber (helpers.ts)

--- src/homework-2/helpers/helpers.ts
+++ src/homework-2/helpers/helpers.ts
@@ -1,2 +1,3 @@
 const regexp = /\S/;
-export const isNumber = (item: string): boolean => regexp.test(item) && !isNaN(Number(item));
+export const isNumber = (item: string): boolean =>
+  regexp.test(item) && !isNaN(Number(item));
