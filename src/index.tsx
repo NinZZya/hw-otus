17d35a7d04c94ec36a609baf32d9ed9f9b814187
ninzzya <s.a.azarov@gmail.com>
2020-10-31 02:47:06 +0300

    Config storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<div>Hello World</div>, document.getElementById("root"));
