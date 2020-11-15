beb789cce741b672c79312bd97cd6b2107d885f4
NinZZya <s.a.azarov@gmail.com>
2020-11-15 23:34:00 +0300

    Review index.jsx, babel and scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-import "./index.css";
+/** @desc jsx */
+import { Global, css } from "@emotion/core";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <Global
+      styles={css`
+        body {
+          width: 100%;
+          height: 100%;
+          margin: 0;
+          padding: 0;
+          background-color: #000;
+          color: #fff;
+          box-sizing: border-box;
+          font-family: Arial, sans-serif;
+          font-size: 14px;
+          line-height: 24px;
+        }
+      `}
+    />
+    <App />
+  </>,
+  document.getElementById("root")
+);
