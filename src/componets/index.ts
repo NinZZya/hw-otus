4039e40b8da99e9d305fbeddaf4dccb1267081d0
NinZZya <s.a.azarov@gmail.com>
2020-12-06 20:43:00 +0300

    Export layouts

--- src/componets/index.ts
+++ src/componets/index.ts
@@ -3,3 +3,5 @@ export * from "./card/card";
 export * from "./charts/bar-chart/bar-chart";
 export * from "./form/form";
 export * from "./inputs/input/input";
+export * from "./layouts/col/col";
+export * from "./layouts/row/row";
