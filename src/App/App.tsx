2636206638310d34900d1b5d726cc0de7fef812c
NinZZya <s.a.azarov@gmail.com>
2020-12-06 17:51:16 +0300

    Add global theme

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+/** @desc desc */
+import { ThemeProvider } from "@emotion/react";
+import { Global, css } from "@emotion/core";
+import darkTheme from "../themes/dark";
 
 export const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Global
+        styles={css`
+          body {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            background-color: ${darkTheme.color.background};
+            color: ${darkTheme.color.text};
+            box-sizing: border-box;
+            font-family: ${darkTheme.font.family};
+            font-size: ${darkTheme.font.size};
+            line-height: ${darkTheme.font.lineHeight};
+          }
+        `}
+      />
+      <div>Hello World</div>
+    </ThemeProvider>
+  );
 };
