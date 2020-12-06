efbcb3ad01cb9982611ec968c7204ac820f5d85a
NinZZya <s.a.azarov@gmail.com>
2020-12-06 17:43:36 +0300

    Add theme file

---
+++ src/themes/dark.ts
@@ -0,0 +1,18 @@
+export default {
+  color: {
+    primary: "#3700b3",
+    secondary: "#2a2a2a",
+    background: "#000000",
+    surface: "#121212",
+    text: "#ffffff",
+    border: "#111111",
+    error: "#cf6679",
+    shadowBorder: "rgba(255, 255, 255, 0.1)",
+    backgroundHover: "rgba(0, 0, 0, 0.1)",
+  },
+  font: {
+    family: "Arial, sans-serif",
+    size: "14px",
+    lineHeight: "24px",
+  },
+};
